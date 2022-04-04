d289af527bfb6fbf6ea76419b8fe709b75ae56ab
diabeteskungen99 <lukasbergman@live.se>
2022-04-04 17:06:50 +0200

    test commit

---
+++ HelloWOrld.java
@@ -0,0 +1,12 @@
+public class HelloWOrld {
+
+
+        public static void main(String[] args) {
+            System.out.println("Hello world");
+            // write your code here
+            Product product = new Product("Keyboard");
+
+            System.out.println(product.getName());
+        }
+    }
+}
